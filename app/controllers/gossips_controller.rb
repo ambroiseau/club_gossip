53b373facac88855dd6dcef6029c95dfa7a4bbc0
des_esseintes <ambroise.victorpujebet@gmail.com>
2018-10-31 20:25:29 +0100

    pas de show après avoir rempli le form

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -1,14 +1,26 @@
 class GossipsController < ApplicationController
+  
+  def home
+  end
+
   def index
   end
 
   def show
+    Gossip.find(params[:id])
   end
 
   def new
+    @gossip = Gossip.new
   end
 
   def create
+    if params[:gossip]
+      gossip = params[:gossip]
+    else
+      gossip = params
+    end
+    Gossip.create(anonymous_gossiper: gossip[:anonymous_gossiper], content: gossip[:content])
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  resources :static_pages
   resources :gossips, except: [:destroy] do
     resources :comments, only: [:new, :create, :index, :destroy]
   end

191752c56edbdc56e3695db102a86c5a4b7936ed
des_esseintes <ambroise.victorpujebet@gmail.com>
2018-11-01 15:29:11 +0100

    version 1.0 sans les comz

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -4,10 +4,11 @@ class GossipsController < ApplicationController
   end
 
   def index
+    @gossips = Gossip.all
   end
 
   def show
-    Gossip.find(params[:id])
+    @gossip = Gossip.find(params[:id])
   end
 
   def new
@@ -15,20 +16,25 @@ class GossipsController < ApplicationController
   end
 
   def create
-    if params[:gossip]
-      gossip = params[:gossip]
-    else
-      gossip = params
-    end
-    Gossip.create(anonymous_gossiper: gossip[:anonymous_gossiper], content: gossip[:content])
+    gossip = params[:gossip]
+    @gossip = Gossip.create(anonymous_gossiper: gossip[:anonymous_gossiper], content: gossip[:content])
+    redirect_to @gossip
   end
 
   def edit
+    @gossip = Gossip.find(params[:id])
   end
 
   def update
+    @gossip = Gossip.find(params[:id])
+    gossip_params = params.require(:gossip).permit(:anonymous_gossiper, :content)
+    @gossip.update(gossip_params)
+    redirect_to gossip_path
   end
 
   def destroy
+    @gossip = Gossip.find(params[:id])
+    @gossip.destroy
+    redirect_to gossips_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   resources :static_pages
-  resources :gossips, except: [:destroy] do
+  resources :gossips do
     resources :comments, only: [:new, :create, :index, :destroy]
   end
 end
